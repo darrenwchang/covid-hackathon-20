730c04c00e03913d2f7eeee6a6d7b1800210e82d
Darren Chang <57879991+darrenwchang@users.noreply.github.com>
2020-06-14 13:27:08 -0400

    Add files via upload

---
+++ QE_model.R
@@ -0,0 +1,11 @@
+library(magrittr)
+library(tidyverse)
+
+setwd("~/Downloads")
+mydata = read.csv("qe2_effects.csv") %>% 
+    as_tibble() %>% 
+    filter(abs(time) <= 10) %>% 
+    mutate(announce = (time > 0))
+
+lm(X1.MO ~ time + announce, mydata) %>% 
+    summary() # repeat for every variable
